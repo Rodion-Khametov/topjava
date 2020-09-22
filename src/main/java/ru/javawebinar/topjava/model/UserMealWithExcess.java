9002eccf46c8b639d1fff309e065c95636bb6872
Rodion <strhodes13@gmail.com>
2020-09-22 13:52:12 +0400

    Prepare to HW0 patch

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.topjava.model;
+
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class UserMealWithExcess {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean excess;
+
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean getExcess() { return excess; }
+
+    @Override
+    public String toString() {
+        return "UserMealWithExcess{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
